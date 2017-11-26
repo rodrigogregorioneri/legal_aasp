ce7c2a0e0d09d15ea4e3ceec1b04aaf2467522a8
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2017-11-26 01:32:26 -0200

    implementacao foto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Camera} from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -7,9 +8,22 @@ import { NavController } from 'ionic-angular';
 
 })
 export class HomePage {
-
+public base64Image: string;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
   }
 
 }
