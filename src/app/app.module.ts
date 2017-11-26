42a31f8842e610a7dfe8144f58566b000588452e
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2017-11-26 01:44:03 -0200

    ajuste appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
